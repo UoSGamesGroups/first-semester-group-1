40f15765f0e575a4d4b774cd440507f8ede7e1c0
AKegge <S186229@uos.ac.uk>
2016-10-31 21:42:59 +0000

    Add files via upload

---
+++ Code/EnemyController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+    Animator anim;
+    public bool isChasingPlayer = true;
+    private GameObject self;
+    private GameObject player;
+    private float distance;
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        self = GameObject.Find("enemy");
+    }
+    // Update is called once per frame
+    void fixedUpdate()
+    {
+        Vector2 movement = Vector2.MoveTowards(self.transform.position, player.transform.position, maxSpeed);
+        anim.SetFloat("Speed", Mathf.Abs(movement.magnitude));
+        distance = Vector2.Distance(self.transform.position, player.transform.position);
+        if (isChasingPlayer)
+        {
+            if (distance > 1)
+            {
+                self.transform.Translate(movement);
+            }
+            else
+            {
+                isChasingPlayer = false;
+            }
+        }
+        else if (!isChasingPlayer)
+        {
+            if (distance > 1)
+            {
+                isChasingPlayer = true;
+            }
+        }
+        if (movement.magnitude > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (movement.magnitude < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Code/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+public class PlayerController : MonoBehaviour
+{
+    //variables
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+    Animator anim;
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float jumpForce = 70f;
+    public bool airMovement = true;
+    public KeyCode jumpKey = KeyCode.Space;
+    //does this at start
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    //does this all the time (does not depend on framerate, less accurate than Update)
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+        anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+        if (!grounded && airMovement == false) { return; }//<- for disableing movement in air
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(move));
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        //changes facing
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+    //does this all the time (depends on framerate, more accurate than FixedUpdate)
+    void Update()
+    {
+        //!!change GetKeyDown to be remappable!!
+        if (grounded && Input.GetKeyDown(jumpKey))
+        {
+            anim.SetBool("Ground", false);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        }
+    }
+    //for changing facing
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
