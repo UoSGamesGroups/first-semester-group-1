97f6a09542da0cfe9dc344e5baac2155103d5674
SorryInAdvance <s176553@ucs.ac.uk>
2016-11-07 22:00:59 +0000

    Add files via upload

---
+++ Code/HolotapeScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HolotapeScript : MonoBehaviour {
+	public GameObject Tape;
+	public GameObject MainCamera;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown() {
+		if (Input.GetMouseButtonDown (0)) {
+			Instantiate (Tape, MainCamera.transform.position, Quaternion.identity);
+		}
+	}
+}
---
+++ Code/TapeMessageScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapeMessageScript : MonoBehaviour {
+
+	void OnMouseDown() {
+		if (Input.GetMouseButtonDown (0)) {
+			Destroy (gameObject);
+		}
+	}
+}
